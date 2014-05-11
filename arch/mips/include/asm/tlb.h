4cee9796397451a5fb93e66c317989e37cdb608b
Richard Weinberger <richard@nod.at>
2014-05-11 17:30:27 +0200

    mips: Use common bits from generic tlb.h
    It is no longer needed to define them on our own.
    
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: John Crispin <blogic@openwrt.org>
    Cc: Markos Chandras <markos.chandras@imgtec.com>
    Cc: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/mips/include/asm/tlb.h
+++ arch/mips/include/asm/tlb.h
@@ -10,13 +10,6 @@
 		if (!tlb->fullmm)				\
 			flush_cache_range(vma, vma->vm_start, vma->vm_end); \
 	}  while (0)
-#define tlb_end_vma(tlb, vma) do { } while (0)
-#define __tlb_remove_tlb_entry(tlb, ptep, address) do { } while (0)
-
-/*
- * .. because we flush the whole mm when it fills up.
- */
-#define tlb_flush(tlb) flush_tlb_mm((tlb)->mm)
 
 #define UNIQUE_ENTRYHI(idx)						\
 		((CKSEG0 + ((idx) << (PAGE_SHIFT + 1))) |		\
