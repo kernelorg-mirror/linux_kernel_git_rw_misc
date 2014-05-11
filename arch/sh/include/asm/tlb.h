c7f6e06b0dc75a3c3494449fac7a7b3313440b54
Richard Weinberger <richard@nod.at>
2014-05-11 17:54:33 +0200

    sh: Use common bits from generic tlb.h
    It is no longer needed to define them on our own.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: linux-sh@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/sh/include/asm/tlb.h
+++ arch/sh/include/asm/tlb.h
@@ -133,11 +133,6 @@ static inline void tlb_unwire_entry(void)
 
 #else /* CONFIG_MMU */
 
-#define tlb_start_vma(tlb, vma)				do { } while (0)
-#define tlb_end_vma(tlb, vma)				do { } while (0)
-#define __tlb_remove_tlb_entry(tlb, pte, address)	do { } while (0)
-#define tlb_flush(tlb)					do { } while (0)
-
 #include <asm-generic/tlb.h>
 
 #endif /* CONFIG_MMU */
