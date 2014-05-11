8f40da73b27af55a3e21d6cbdf592c5896cbde06
Richard Weinberger <richard@nod.at>
2014-05-11 17:37:42 +0200

    parisc: Use common bits from generic tlb.h
    It is no longer needed to define them on our own.
    
    Cc: "James E.J. Bottomley" <jejb@parisc-linux.org>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: linux-parisc@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/parisc/include/asm/tlb.h
+++ arch/parisc/include/asm/tlb.h
@@ -16,12 +16,6 @@ do {	if (!(tlb)->fullmm)	\
 		flush_tlb_range(vma, vma->vm_start, vma->vm_end); \
 } while (0)
 
-#define __tlb_remove_tlb_entry(tlb, pte, address) \
-	do { } while (0)
-
 #include <asm-generic/tlb.h>
-
-#define __pmd_free_tlb(tlb, pmd, addr)	pmd_free((tlb)->mm, pmd)
-#define __pte_free_tlb(tlb, pte, addr)	pte_free((tlb)->mm, pte)
 
 #endif
