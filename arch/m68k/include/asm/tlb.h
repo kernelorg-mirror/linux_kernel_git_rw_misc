3b3dd26d2d7571f895c69f7d42a306c1a823f7d7
Richard Weinberger <richard@nod.at>
2014-05-13 23:09:10 +0200

    m68k: Use common bits from generic tlb.h
    It is no longer needed to define them on our own.
    
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: linux-m68k@lists.linux-m68k.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/m68k/include/asm/tlb.h
+++ arch/m68k/include/asm/tlb.h
@@ -1,19 +1,11 @@
 #ifndef _M68K_TLB_H
 #define _M68K_TLB_H
 
-/*
- * m68k doesn't need any special per-pte or
- * per-vma handling..
- */
-#define tlb_start_vma(tlb, vma)	do { } while (0)
-#define tlb_end_vma(tlb, vma)	do { } while (0)
-#define __tlb_remove_tlb_entry(tlb, ptep, address)	do { } while (0)
-
-/*
- * .. because we flush the whole mm when it
- * fills up.
- */
-#define tlb_flush(tlb)		flush_tlb_mm((tlb)->mm)
+#ifdef CONFIG_MMU && !defined(CONFIG_COLDFIRE) && !defined(CONFIG_SUN3)
+/* These defines are needed to override the defaults from asm-generic/tlb.h */
+#define __pte_free_tlb __pte_free_tlb
+#define __pmd_free_tlb __pmd_free_tlb
+#endif
 
 #include <asm-generic/tlb.h>
 
