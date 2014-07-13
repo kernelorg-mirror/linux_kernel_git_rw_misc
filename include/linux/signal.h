85d1af820898403700a15287c58aa3022980a96b
Richard Weinberger <richard@nod.at>
2014-07-13 13:36:04 +0200

    Clean up signal_delivered()
     - Pass a ksignal struct to it
     - Remove unused regs parameter
     - Make it private as it's nowhere outside of kernel/signal.c is used
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- include/linux/signal.h
+++ include/linux/signal.h
@@ -282,7 +282,6 @@ struct ksignal {
 
 extern int get_signal(struct ksignal *ksig);
 extern void signal_setup_done(int failed, struct ksignal *ksig, int stepping);
-extern void signal_delivered(int sig, siginfo_t *info, struct k_sigaction *ka, struct pt_regs *regs, int stepping);
 extern void exit_signals(struct task_struct *tsk);
 extern void kernel_sigaction(int, __sighandler_t);
 
