fbdaf9ec63efe111094ef8c790338bf46b95493c
Richard Weinberger <richard@nod.at>
2014-10-25 12:55:03 +0200

    UBI: Fastmap: Fix race after ubi_wl_get_peb()
    ubi_wl_get_peb() returns a fresh PEB which can be used by
    user of UBI. Due to the pool logic fastmap will correctly
    map this PEB upon attach time because it will be scanned.
    
    If a new fastmap is written (due to heavy parallel io)
    while the before the fresh PEB is assigned to the EBA table
    it will not be scanned as it is no longer in the pool.
    So, the race window exists between ubi_wl_get_peb()
    and the EBA table assignment.
    We have to make sure that no new fastmap can be written
    while that.
    
    To ensure that ubi_wl_get_peb() will grab ubi->fm_sem in read mode
    and the user of ubi_wl_get_peb() has to release it after the PEB
    got assigned.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- drivers/mtd/ubi/wl.c
+++ drivers/mtd/ubi/wl.c
@@ -628,6 +628,7 @@ void ubi_refill_pools(struct ubi_device *ubi)
 
 /* ubi_wl_get_peb - works exaclty like __wl_get_peb but keeps track of
  * the fastmap pool.
+ * Returns with ubi->fm_sem held in read mode!
  */
 int ubi_wl_get_peb(struct ubi_device *ubi)
 {
@@ -636,15 +637,19 @@ int ubi_wl_get_peb(struct ubi_device *ubi)
 	struct ubi_fm_pool *wl_pool = &ubi->fm_wl_pool;
 
 again:
+	down_read(&ubi->fm_sem);
 	spin_lock(&ubi->wl_lock);
 	if (!pool->size || !wl_pool->size || pool->used >= pool->size ||
 	    wl_pool->used >= wl_pool->size) {
 		spin_unlock(&ubi->wl_lock);
+		up_read(&ubi->fm_sem);
 		ret = ubi_update_fastmap(ubi);
 		if (ret) {
 			ubi_msg("Unable to write a new fastmap: %i", ret);
+			down_read(&ubi->fm_sem);
 			return -ENOSPC;
 		}
+		down_read(&ubi->fm_sem);
 		spin_lock(&ubi->wl_lock);
 	}
 
@@ -721,6 +726,7 @@ int ubi_wl_get_peb(struct ubi_device *ubi)
 		return err;
 	}
 
+	down_read(&ubi->fm_sem);
 	return peb;
 }
 #endif
