cb68233b0c81bacd69ea4282f25497b56d47533e
Richard Weinberger <richard@nod.at>
2014-10-29 11:44:45 +0100

    UBI: Fastmap: Remove else after return.
    checkpatch.pl complains:
    WARNING: else is not generally useful after a break or return
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- drivers/mtd/ubi/fastmap-wl.c
+++ drivers/mtd/ubi/fastmap-wl.c
@@ -253,10 +253,10 @@ static struct ubi_wl_entry *get_peb_for_wl(struct ubi_device *ubi)
 			schedule_work(&ubi->fm_work);
 		}
 		return NULL;
-	} else {
-		pnum = pool->pebs[pool->used++];
-		return ubi->lookuptbl[pnum];
 	}
+
+	pnum = pool->pebs[pool->used++];
+	return ubi->lookuptbl[pnum];
 }
 
 /**
