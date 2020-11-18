3ce40438bad71d46358f8621fb2556344d748bd1
Richard Weinberger <richard@nod.at>
2020-11-18 23:06:25 +0100

    mtd: Add MTD_MUSE flag
    This flag will be set if an MTD is implemeted in userspace
    using MUSE.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- include/uapi/mtd/mtd-abi.h
+++ include/uapi/mtd/mtd-abi.h
@@ -105,6 +105,7 @@ struct mtd_write_req {
 #define MTD_NO_ERASE		0x1000	/* No erase necessary */
 #define MTD_POWERUP_LOCK	0x2000	/* Always locked after reset */
 #define MTD_SLC_ON_MLC_EMULATION 0x4000	/* Emulate SLC behavior on MLC NANDs */
+#define MTD_MUSE		0x8000 /* This MTD is implemented in userspace */
 
 /* Some common devices / combinations of capabilities */
 #define MTD_CAP_ROM		0
